011980edae0701a941212bcac12cbb830e88f75d
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2020-02-21 23:06:31 +0200

    print inorder added

--- Final_Collector.py
+++ Final_Collector.py
@@ -10,8 +10,15 @@ for Publisher in sys.argv[2:]:
 receiverSocket = configure_Subscriber(Publishers)
 f = open(sys.argv[1], "w")
 
+orderNum = 1
+dictionaryFrames = {}
 while True:
     img = pickle.loads(receiverSocket.recv())
-    f.write("Frame# {}:\nXmin =  {}, Xmax = {},Ymin =  {}, Ymax = {} \n".format(
-        img["frameNum"], img["Xmin"], img["Xmax"], img["Ymin"], img["Ymax"]))
-    f.write("--------------------------------------------------------\n")
+    # To print inorder
+    dictionaryFrames[img["frameNum"]] = img
+    if(img["frameNum"] == orderNum):
+        imgToOut = dictionaryFrames[img["frameNum"]]
+        f.write("Frame# {}:\nXmin =  {}, Xmax = {},Ymin =  {}, Ymax = {} \n".format(
+            imgToOut["frameNum"], imgToOut["Xmin"], imgToOut["Xmax"], imgToOut["Ymin"], imgToOut["Ymax"]))
+        f.write("--------------------------------------------------------\n")
+        orderNum += 1
